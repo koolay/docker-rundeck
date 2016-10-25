d3cf449c6b6cd6d525a544a3f683b5eac7dbb104
koolay <koolay@163.com>
2016-10-25 14:41:55 +0800

    run as user rundeck

--- rundeck.sh
+++ rundeck.sh
@@ -1,16 +1,5 @@
 #!/bin/sh
 set -e
 
-. /lib/lsb/init-functions
-. /etc/rundeck/profile
-
-prog="rundeckd"
-RETVAL=0
-PIDFILE=/var/run/$prog.pid
-DAEMON="${JAVA_HOME:-/usr}/bin/java"
-DAEMON_ARGS="${RDECK_JVM} -cp ${BOOTSTRAP_CP} com.dtolabs.rundeck.RunServer /var/lib/rundeck ${RDECK_HTTP_PORT}"
-rundeckd="$DAEMON $DAEMON_ARGS"
-
-echo -n $"Starting $prog: "
-exec /sbin/setuser rundeck /bin/bash rundeck -c "$rundeckd" &>>/dev/stdout 2>&1
-log_success_msg
+echo "----start rundeck----"
+java -Xmx1024m -jar $RDECK_BIN -c /etc/rundeck -b $RDECK_BASE >> /dev/stdout 2>&1
