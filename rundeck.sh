61524666bbe4618873e81a2369bad0236104d606
koolay <koolay@163.com>
2016-11-24 14:21:39 +0800

    setting htt port

--- rundeck.sh
+++ rundeck.sh
@@ -2,4 +2,4 @@
 set -e
 
 echo "----start rundeck----"
-java -Xmx1024m -jar $RDECK_BIN -c /etc/rundeck -b $RDECK_BASE >> /dev/stdout 2>&1
+java -Dserver.http.port=$SERVER_PORT -Xmx1024m -jar $RDECK_BIN -c /etc/rundeck -b $RDECK_BASE >> /dev/stdout 2>&1
